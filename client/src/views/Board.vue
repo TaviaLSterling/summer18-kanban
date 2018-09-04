2112761242daf8290de8bb9b80499e3faac791c6
charliesands <cwsands3@gmail.com>
2018-09-04 16:47:44 -0600

    in progress

--- client/src/views/Board.vue
+++ client/src/views/Board.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="board">
     {{boardId}}
+    <router-link :to="{name: 'board', params: {boardId: board._id}}">{{board.title}}</router-link>
   </div>
 </template>
 
 <script>
-export default {
-  name: "board",
-  created() {
-    //blocks users not logged in
-    if (!this.$store.state.user._id) {
-      this.$router.push({ name: "login" });
-    }
-  },
-  props: ["boardId"]
-};
+  export default {
+    name: "board",
+    created() {
+      //blocks users not logged in
+      if (!this.$store.state.user._id) {
+        this.$router.push({ name: "login" });
+      }
+    },
+    props: ["boardId"],
+    computed: {
+      activeBoard() {
+        return this.$store.state.activeBoard;
+      }
+    },
+  };
 </script>
